31eeb7c73ce9ab32ff8722842b200155a53a37fe
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-28 02:07:42 +0530

    Show: Initial comment

---
+++ DataStructureProgram/src/Com/DataStructureProblem/DataStructureProblem.java
@@ -0,0 +1,20 @@
+/**
+* <h1>Welcome to the data structure problem !!</h1> 
+* The Data Structure Program implements an application that
+* simply displays "Welcome to the data structure problem !!" to the standard output.
+* 
+* @author Official_vk
+* @version 1.0
+* @since 2021-07-28
+*/
+
+package Com.DataStructureProblem;
+
+public class DataStructureProblem {
+
+	public static void main(String[] args) {
+		// Prints Welcome to the data structure problem !! on standard output.
+		System.out.println("Welcome to the data structure problem !!");
+	}
+
+}
